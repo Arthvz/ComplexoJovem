0ade2908c9a6e59e4233fda1031049dc439bf529
Arthur Verdadeiro <arthurverdadeiro@outlook.com>
2021-11-16 09:25:22 -0300

    Update map.js

--- map.js
+++ map.js
@@ -6,21 +6,7 @@ function initMap() {
       zoom: 12,
       center: myLatLng,
     });
-
-    var beachMarker = new google.maps.Marker({
-        position: new google.maps.LatLng(-23.648726, -46.847502),
-        map: map,
-        icon: 'brown_marker.png',
-        title: "Pronto Socorro Central (Insatisfação)",
-      });
-
-    var beachMarker = new google.maps.Marker({
-      position: new google.maps.LatLng(-23.630100572243702,-46.82668089866638),
-      map: map,
-      icon: 'green_marker.png',
-      title: "Rua Anápolis (Queria que aqui não fosse...)",
-    });
-
+    
     var beachMarker = new google.maps.Marker({
         position: new google.maps.LatLng(-23.619627,-46.771811),
         map: map,
@@ -34,4 +20,53 @@ function initMap() {
         icon: 'red_marker.png',
         title: "EE Amélia dos Anjos Oliveira (Locais de Agressão)",
       });
+    
+    var beachMarker = new google.maps.Marker({
+        position: new google.maps.LatLng(-23.58573961427639,-46.744582176208496),
+        map: map,
+        icon: 'red_marker.png',
+        title: "A mata (Locais de Agressão)",
+      });
+    
+    var beachMarker = new google.maps.Marker({
+        position: new google.maps.LatLng(-23.592858893816615,-46.731961509579826),
+        map: map,
+        icon: 'red_marker.png',
+        title: "Emef Arthur Whitaker (Locais de Agressão)",
+      });
+    
+    var beachMarker = new google.maps.Marker({
+        position: new google.maps.LatLng(-23.57973109465769,-46.74542447179543),
+        map: map,
+        icon: 'red_marker.png',
+        title: "Ponto 4 (Locais de Agressão)",
+      });
+    
+    var beachMarker = new google.maps.Marker({
+        position: new google.maps.LatLng(-23.662990610128766,-46.79589254234524),
+        map: map,
+        icon: 'red_marker.png',
+        title: "Rua Moura (Locais de Agressão)",
+      });
+    
+    var beachMarker = new google.maps.Marker({
+        position: new google.maps.LatLng(-23.631064050595043,-46.759146266287814),
+        map: map,
+        icon: 'red_marker.png',
+        title: "Ponto 7 (Locais de Agressão)",
+      });
+    
+     var beachMarker = new google.maps.Marker({
+        position: new google.maps.LatLng(-23.648726, -46.847502),
+        map: map,
+        icon: 'brown_marker.png',
+        title: "Pronto Socorro Central (Insatisfação)",
+      });
+
+    var beachMarker = new google.maps.Marker({
+      position: new google.maps.LatLng(-23.630100572243702,-46.82668089866638),
+      map: map,
+      icon: 'green_marker.png',
+      title: "Rua Anápolis (Queria que aqui não fosse...)",
+    });
 }
